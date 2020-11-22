8323023506a6673f65e2799fbef177571796da02
zyy <pm_zyy@163.com>
2020-11-22 14:03:44 +0800

    add article_create and article_delete

--- article/urls.py
+++ article/urls.py
@@ -6,4 +6,6 @@ app_name = 'article'
 urlpatterns = [
     path('article_list/', views.article_list, name='article_list'),
     path('article_detail/<int:id>', views.article_detail, name='article_detail'),
+    path('article_create/', views.article_create, name='article_create'),
+    path('article_delete/<int:id>', views.article_delete, name='article_delete'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import ArticlePost
+from django.contrib.auth.models import User
 import markdown
+from django.http import HttpResponse
+from .forms import ArticlePostForm
+
+
 # Create your views here.
 def article_list(request):
     # 从ArticlePost取出所有文章
@@ -24,3 +29,24 @@ def article_detail(request, id):
     context = { 'article': article }
 
     return render(request, 'article/detail.html', context)
+
+def article_create(request):
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=1)
+            new_article.save()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    else:
+        article_post_form = ArticlePostForm()
+        context = { 'article_post_form': article_post_form }
+        return render(request, 'article/create.html', context)
+
+
+def article_delete(request, id):
+    article = ArticlePost.objects.get(id=id)
+    article.delete()
+    return redirect("article:article_list")
