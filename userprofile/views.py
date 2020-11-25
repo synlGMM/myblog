ccfda4c9b240cdcc819591018f51a10c25dab120
zyy <pm_zyy@163.com>
2020-11-25 11:16:49 +0800

    add user_delete

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegisterForm
+
+from django.contrib.auth.models import User
+# 引入验证登录的装饰器
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def user_login(request):
@@ -30,3 +34,35 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("article:article_list")
+
+def user_register(request):
+    if request.method == 'POST':
+        user_register_form = UserRegisterForm(data=request.POST)
+        if user_register_form.is_valid():
+            new_user = user_register_form.save(commit=False)
+            new_user.set_password(user_register_form.cleaned_data['password'])
+            new_user.save()
+            login(request, new_user)
+            return redirect('article:article_list')
+        else:
+            return HttpResponse('注册表单有误。')
+    elif request.method == 'GET':
+        user_register_form = UserRegisterForm()
+        context = { 'form': user_register_form }
+        return render(request, 'userprofile/register.html', context)
+    else:
+        return HttpResponse('请使用GET或POST请求数据')
+
+@login_required(login_url='userprofile/login/')
+def user_delete(request, id):
+    if request.method == 'POST':
+        user = User.objects.get(id=id)
+        # 验证登录用户与待删除的用户是否相同
+        if user == request.user:
+            logout(request)
+            user.delete()
+            return redirect('article:article_list')
+        else:
+            return HttpResponse('你没有删除操作的权限')
+    else:
+        return HttpResponse('仅接受POST请求')
